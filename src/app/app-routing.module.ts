8c5be2a3fb22f57131402ab9f276d5171376881d
Berni <bernalpas@gmail.com>
2021-10-20 15:44:15 -0300

    Practicando Deploy de Aplicación en Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './paginas/about/about.component';
+import { ItemsComponent } from './paginas/items/items.component';
+import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'items', component: ItemsComponent },
+  { path: 'home', component: PortafolioComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
+
+
+
 export class AppRoutingModule { }
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+
+export interface InfoPagina{
+    titulo?: string;
+    email?: string;
+    nombre?: string;
+    pagina?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?:string;
+    tumblr?: string;
+}
